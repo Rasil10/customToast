a1ecf2ad51a344928d301f907211c2d9de6d64f1
Rasil <Rasil>
2022-03-21 11:07:41 +0545

    Added Custom Toast View

--- CustomToast/src/main/java/com/rasil/customtoast/CustomToast.kt
+++ CustomToast/src/main/java/com/rasil/customtoast/CustomToast.kt
@@ -1,10 +1,17 @@
 package com.rasil.customtoast
 
 import android.content.Context
-import android.widget.Toast
 
 class CustomToast {
-    fun show(c: Context?, message: String?) {
-        Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
+    fun showSuccessToast(c: Context?, message: String?) {
+        CustomToastHelper(c!!).showSuccessToast(message)
+    }
+
+    fun showInfoToast(c: Context?, message: String?) {
+        CustomToastHelper(c!!).showInfoToast(message)
+    }
+
+    fun showErrorToast(c: Context?, message: String?) {
+        CustomToastHelper(c!!).showErrorToast(message)
     }
 }
