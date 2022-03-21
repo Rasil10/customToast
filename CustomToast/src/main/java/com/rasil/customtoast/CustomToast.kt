76dc0c0a7b429dfc0b8f1cd04d88ecd097c59f53
Rasil <Rasil>
2022-03-21 10:46:27 +0545

    Added simple Toast

--- CustomToast/src/main/java/com/rasil/customtoast/CustomToast.kt
+++ CustomToast/src/main/java/com/rasil/customtoast/CustomToast.kt
@@ -1,4 +1,10 @@
 package com.rasil.customtoast
 
+import android.content.Context
+import android.widget.Toast
+
 class CustomToast {
+    fun show(c: Context?, message: String?) {
+        Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
+    }
 }
